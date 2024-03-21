8fae21b7a015110dd32ea8898598669d2bccbbf8
William-Kwan21 <william.kwan2@cix.csi.cuny.edu>
2024-03-20 20:59:34 -0400

    putting codes and pictures in folders

---
+++ Lab 6/script.js
@@ -0,0 +1,27 @@
+document.getElementById('loadFacts').addEventListener('click', function() {
+    fetch('https://brianobruno.github.io/cats.json')
+        .then(response => response.json())
+        .then(data => {
+            // Sort facts by factId
+            data.facts.sort((a, b) => a.factId - b.factId);
+
+            // Populate table
+            const table = document.getElementById('factsTable');
+            // Clear existing table rows except the header
+            table.innerHTML = '<tr><th>Fact ID</th><th>Fact</th></tr>';
+            data.facts.forEach(fact => {
+                const row = table.insertRow(-1);
+                const cell1 = row.insertCell(0);
+                const cell2 = row.insertCell(1);
+                cell1.innerHTML = fact.factId;
+                cell2.innerHTML = fact.text;
+            });
+
+            // Update image
+           
+            const imgElement = document.getElementById('catPhoto');
+            imgElement.src = data.catPhoto;
+            console.log(data.catPhoto)
+        })
+        .catch(error => console.error('Error fetching data: ', error));
+});
